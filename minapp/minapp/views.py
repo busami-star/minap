58aac9936f088270530cbd920bd8fc605c8c8fa1
Busami Victor <victorbusami1@gmail.com>
2025-01-30 23:57:15 +0300

    initial commit

--- minapp/minapp/views.py
+++ minapp/minapp/views.py
@@ -1,7 +1,14 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+#
+# def homepage(request):
+#     return HttpResponse("Hello world")
+#
+# def about(request):
+#     return HttpResponse("My about page")
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("Hello world")
+    return render(request,'home.html')
 
 def about(request):
-    return HttpResponse("My about page")
+    return render(request,'about.html')
